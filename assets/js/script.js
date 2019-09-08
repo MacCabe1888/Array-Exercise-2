5615a8538aa0f0bc2455fdc846482d81b1ff3c90
Cameron <cameronjmaccabe@gmail.com>
2019-09-07 19:48:40 -0700

    completed JavaScript array method exercises

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,12 +15,25 @@ const comments = [
 
 // "Some" and "Every" Checks
 // Array.prototype.some() // Is at least one person 19 or older?
+const someNineteenPlus = people.some(person => (new Date()).getFullYear() - person.year >= 19);
+console.log({someNineteenPlus});
 // Array.prototype.every() // Is everyone 19 or older?
+const allNineteenPlus = people.every(person => (new Date()).getFullYear() - person.year >= 19);
+console.log({allNineteenPlus});
 
 // Array.prototype.find()
 // "Find" is like "filter," but instead returns just the one you are looking for.
 // Find the comment with the ID of 823423.
+const findComment = commentID => comments.find(comment => comment.id == commentID);
+const comment = findComment(823423);
+console.log(comment);
 
 // Array.prototype.findIndex()
 // Find the comment with the ID of 823423.
+const findCommentIndex = commentID => comments.findIndex(comment => comment.id == commentID);
+const index = findCommentIndex(823423);
+console.log(index);
 // Delete the comment with this ID.
+const updatedComments = [...comments];
+updatedComments.splice(index, 1);
+console.table(updatedComments);
